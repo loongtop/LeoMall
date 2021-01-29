7006052244a5ac7fbff97e4d99dab36c426f1ff9
leo <leo>
2021-01-28 21:39:55 -0500

    项目初始结构创建first

---
+++ gulimall-order/src/test/java/com/gkhy/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkhy.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/gkhy/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkhy.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
