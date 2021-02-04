4b356d5d2ab7261ed84742b88b0ec794cdea510c
leo <leo>
2021-02-03 21:05:19 -0500

    项目初始结构创建first

---
+++ gulimall-order/src/test/java/com/gkhy/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkhy.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
